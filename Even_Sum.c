32d41a7be6c71a459faead159b0854781dfb499b
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2023-12-15 11:08:05 +0530

    #include<stdio.h> int main() {     int n,esum=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             esum=esum+arr[i];         }     }     printf("%d",esum); }

---
+++ Even_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,esum=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            esum=esum+arr[i];
+        }
+    }
+    printf("%d",esum);
+}
