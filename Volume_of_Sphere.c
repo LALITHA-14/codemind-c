34b2a623484bbc355e88d38060035633b95288c3
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-03-14 21:45:20 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     float vol=1.333333*3.14*a*a*a;     printf("%.2f",vol); }

--- Volume_of_Sphere.c
+++ Volume_of_Sphere.c
@@ -1,10 +1,8 @@
-#include<iostream>
-#include<iomanip>
-using namespace std;
+#include<stdio.h>
 int main()
 {
     int a;
-    cin>>a;
+    scanf("%d",&a);
     float vol=1.333333*3.14*a*a*a;
-    cout<<std::fixed<<setprecision(2)<<vol;
+    printf("%.2f",vol);
 }
