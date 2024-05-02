a2847ff29b8705f902957ab956f360b8eda95f09
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-02 22:43:35 +0530

    #include<stdio.h>

---
+++ Giant_Wheel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+   int x;
+   scanf("%d",&x);
+   if(x>=60)
+   {
+       printf("YES");
+   }
+   else
+   {
+       printf("NO");
+   }
+}
+
