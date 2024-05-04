92fae8047080f3735a7e1f951755b6f95ae113ce
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-04 21:13:35 +0530

    #include<stdio.h>

---
+++ Adivitiya.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    scanf("%d",&n);
+    if(n>=16 && n<=18)
+    {
+        printf("ADVITIYA");
+    }
+    else
+    {
+        printf("WAITING FOR ADVITIYA");
+    }
+}
+
