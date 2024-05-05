6c3f140623b330420b6eeb8ff9f50ae9838aa691
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-05 21:46:54 +0530

    #include<stdio.h>

---
+++ Reach_5_Star.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int chef=x+y;
+    if(chef>=2000)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+}
+
