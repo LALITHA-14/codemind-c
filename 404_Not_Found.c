437e135ecc88543a618b8b2fcc56615a4525a36f
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-07 13:38:35 +0530

    #include<stdio.h>

---
+++ 404_Not_Found.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int x;
+    scanf("%d",&x);
+    if(x==404)
+    {
+        printf("NOT FOUND");
+    }
+    else
+    {
+        printf("FOUND");
+    }
+}
+
