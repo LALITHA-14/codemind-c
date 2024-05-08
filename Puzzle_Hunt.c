d1770d40512d3ee609c3ab32b0aa4532b82921a1
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-08 21:59:45 +0530

    #include<stdio.h>

---
+++ Puzzle_Hunt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int n;
+    scanf("%d",&n);
+    if(n>=6 && n<=8)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
+
