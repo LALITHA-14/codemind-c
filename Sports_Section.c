c73dcb69f130252c7e27b62e3294bbc02a87ac6b
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-08 21:16:06 +0530

    #inclu

---
+++ Sports_Section.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int x;
+    scanf("%d",&x);
+    if(x>=8 && x<=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
+
