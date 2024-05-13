a9b994f2a03ef6ec70ff4561396f1c2c39a12ddf
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-13 22:30:28 +0530

    #include<stdio.h>

---
+++ Ageing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    int i;
+    for(i=0;i<t;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        printf("%d\n",x-10);
+    }
+}
+
