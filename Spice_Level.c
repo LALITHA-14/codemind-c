c21bacee14492748fbf7aa22ee9b6e39d45d83db
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-14 10:28:40 +0530

    #include<stdio.h>

---
+++ Spice_Level.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x<4)
+        {
+            printf("MILD\n");
+        }
+        else if(x>=4 && x<7)
+        {
+            printf("MEDIUM\n");
+        }
+        else
+        {
+            printf("HOT\n");
+        }
+    }
+}
+
