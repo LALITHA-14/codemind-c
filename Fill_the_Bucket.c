3804210edc609e0750e0f47be77cf7c4d93def44
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-15 20:03:06 +0530

    #include<stdio.h>

---
+++ Fill_the_Bucket.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int k,x;
+        scanf("%d%d",&k,&x);
+        printf("%d\n",k-x);
+    }
+}
+
