db56194fd6f1cdc816fbcd47518d1ae6d8beaa52
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-15 20:52:24 +0530

    #include<stdio.h>

---
+++ Money_Double.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y,i;
+        scanf("%d%d",&x,&y);
+        for(i=0;i<y;i++)
+    {
+        if((x+1000)>(2*x))
+        {
+            x=x+1000;
+        }
+        else
+        {
+            x=x*2;
+        }
+    }
+     printf("%d\n",x);
+    }
+}
+
