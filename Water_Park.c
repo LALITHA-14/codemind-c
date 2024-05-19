eea650164180600e5485f57069602eb23920204a
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-19 20:51:20 +0530

    #include<stdio.h>

---
+++ Water_Park.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    int w,h;
+    scanf("%d%d",&w,&h);
+    if(w>=60 && h<=130)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+}
+
