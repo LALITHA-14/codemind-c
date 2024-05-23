2da15ec85503e1ca90318d59e2c28f942ad0c464
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-23 22:40:29 +0530

    #include<stdio.h>

---
+++ Janmansh_and_Coins.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int a=(x*10)+(y*5);
+        printf("%d\n",a);
+    }
+}
+
