c732e695afd32af5a6d20332476dc631d2be3f6e
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-24 20:23:03 +0530

    #include<stdio.h>

---
+++ CRED_Coins.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int a=x*y;
+        printf("%d\n",a/100);
+    }
+}
+
