5b946401ab126f781c88fb9793e8516c64b51171
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-24 20:19:22 +0530

    #include<stdio.h>

---
+++ Chef_in_his_Office.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+	// your code goes here
+ int t;
+ scanf("%d",&t);
+ int i;
+ for(i=0;i<t;i++)
+ {
+     int x,y;
+     scanf("%d%d",&x,&y);
+     int a=(x*4)+(y*1);
+    printf("%d\n",a);
+ }
+}
+
