57539934f47d60ed1220acd3752ec66c27f8ff49
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-05-29 21:56:04 +0530

    #include<stdio.h>

---
+++ Even_Sum_Subarray.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int i,n;
+        scanf("%d",&n);
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        int max=0,cur=0,s=0,j;
+        for(i=0;i<n;i++)
+        {
+            s=0;
+            for(j=i;j<n;j++)
+            {
+                s+=a[j];
+                if(s%2==0)
+                {
+                    cur=j-i+1;
+                    if(cur>max)
+                    {
+                        max=cur;
+                    }
+                }
+            }
+        }
+        printf("%d\n",max);
+}
+}
