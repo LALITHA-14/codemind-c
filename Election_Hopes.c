4e21f85a673c90107187edf0b098a40b158e53c6
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-05 21:22:53 +0530

    #include<stdio.h>

---
+++ Election_Hopes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int d=y*2;
+    if(x>=d)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
+
