d1b87287ea070da3a9ea51eaff5097df1ee294a2
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-11 14:16:44 +0530

    #include<stdio.h>

---
+++ Fitness.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int x;
+        scanf("%d", &x);
+        int c=2*5*x;
+        printf("%d\n",c);
+    }  
+}
