830e6950259b54b3b5b23fc5b9a2742c6b0b9081
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-12 20:50:39 +0530

    #include<stdio.h>

---
+++ Long_Drive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int it=10,d=100;
+        int dist=x*it;
+        int min=y*10-dist;
+        int n=100-y;
+        int res=(min+n-1)/n;
+        printf("%d\n",res);
+}
+}
