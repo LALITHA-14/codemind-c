61945f011f3dc563673b73017ab3e6abca45ab08
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-19 21:23:36 +0530

    #include<stdio.h>

---
+++ Distribute_Cookies.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+	int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        scanf("%d%d",&n,&m);
+        if(m<n)
+        {
+            printf("%d\n",n-m);
+        }
+        else
+        {
+            int r=m%n;
+            int add=n-r, rem=r;
+            int min=add;
+            if(rem<add)
+            min=rem;
+            printf("%d\n",min);
+        }
+    }
+}
