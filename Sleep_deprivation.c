1e09f529a3b6096cb126fa22adfb8a70fa621ecc
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-20 19:33:22 +0530

    #include<stdio.h>

---
+++ Sleep_deprivation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x;
+        scanf("%d",&x);
+        if(x<7){
+            printf("YES\n");
+        }
+        else{
+            printf("NO\n");
+        }
+    }
+}
+
