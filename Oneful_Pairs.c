3015158190c9c6798c9f142afdc0f75f5f204e7c
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-25 21:02:09 +0530

    #include<stdio.h>

---
+++ Oneful_Pairs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int c=a+b+(a*b);
+    if(c==111)
+    printf("Yes\n");
+    else
+    {
+        printf("No\n");
+    }
+}
