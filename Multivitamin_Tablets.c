b3490d49b7224a967798c18093f86927976dacf6
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-27 09:44:48 +0530

    #include<stdio.h>

---
+++ Multivitamin_Tablets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int a=x*3;
+        if(a<=y){
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+}
+
