7f1bfba7e1b476e09926870fa0f1e2679861cf4f
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-06-27 10:15:47 +0530

    #include<stdio.h>

---
+++ Worth_of_a_Video.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int s;
+        scanf("%d",&s);
+        int a=s*24;
+        printf("%d\n",a*1000);
+    }
+}
+
