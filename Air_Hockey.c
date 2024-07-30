974d48152ef00019788a990e69c6f8c298f1cded
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-07-30 20:22:35 +0530

    #include<stdio.h>

---
+++ Air_Hockey.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(a>=b){
+            printf("%d\n",7-a);
+        }
+        else{
+            printf("%d\n",7-b);
+        }
+    }
+}
+
