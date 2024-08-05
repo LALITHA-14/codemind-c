fa874d3b7bc29802ca4b9867e5402874f2b04fe2
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-08-05 10:59:33 +0530

    #

---
+++ Subscriptions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int n,x;
+        scanf("%d%d",&n,&x);
+        if(n%6==0){
+            printf("%d\n",(n/6)*x);
+        }
+        else{
+            printf("%d\n",((n/6)+1)*x);
+        }
+    }
+}
+
