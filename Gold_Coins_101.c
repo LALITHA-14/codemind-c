b6232ddb30f6d478367a8e3c10f2e029bd237d79
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-08-14 21:40:23 +0530

    #include<stdio.h>

---
+++ Gold_Coins_101.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int a,b,x,y;
+    scanf("%d%d%d%d",&a,&b,&x,&y);
+    if(x>y){
+        printf("%d",a);
+    }
+    else{
+        printf("%d",b);
+    }
+}
+
