af041e0e733c361e05d0909e6a57b04391111662
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2024-09-25 05:46:18 +0530

    #include<stdio.h>

---
+++ Multiplication_Table.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+       int n,i=1;
+       scanf("%d",&n);
+       int sum=0;
+       while(i<=10){
+           printf("%d X %d = n*i",n,i,n*i);
+           i++;
+        }
+}
