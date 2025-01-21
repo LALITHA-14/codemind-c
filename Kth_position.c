8fe7d84d57cd16b604cf359385f658194e3f3ec6
LALITHA-14 <147792277+LALITHA-14@users.noreply.github.com>
2025-01-21 20:52:07 +0530

    #include<stdio.h>

---
+++ Kth_position.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void insertAfterK(LinkedList* list, int value, int k) {
+    Node* newNode = createNode(value);
+    Node* current = list->head;
+    
+    // If there are no nodes in the linked list
+    // Set the new node as head and return
+    if (current == NULL) {
+        list->head = newNode;
+        return;
+    }
+    
+    // Iterate to the k-th node
+    for (int i = 1; i < k; i++) {
+        current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+    // Set the next of new Node to next of current
+    // Set the next of current to new Node
+}
